db78cbb87e53158959a645009dd22e5b52915888
JRamonCarralero <grukduz@gmail.com>
2026-04-20 23:38:51 +0200

    feat: :sparkles: Scheduler Handler

--- internal/infrastructure/api/dtos/scheduler.go
+++ internal/infrastructure/api/dtos/scheduler.go
@@ -1,6 +1,10 @@
 package dtos
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type CreateSchedulerDTO struct {
 	TrackID   uuid.UUID `json:"track_id" binding:"required"`
@@ -16,12 +20,12 @@ type UpdateSchedulerDTO struct {
 	Room      *string `json:"room" binding:"omitempty"`
 }
 
-type ScheduleResponse struct {
+type SchedulerResponse struct {
 	ScheduleID uuid.UUID `json:"schedule_id"`
 	TrackID    uuid.UUID `json:"track_id"`
 	TalkID     uuid.UUID `json:"talk_id"`
-	StartTime  string    `json:"start_time"`
-	EndTime    string    `json:"end_time"`
+	StartTime  time.Time `json:"start_time"`
+	EndTime    time.Time `json:"end_time"`
 	Room       string    `json:"room"`
 	AuditDTO
 }
